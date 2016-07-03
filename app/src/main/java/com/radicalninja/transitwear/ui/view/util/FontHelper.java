406c5ec2123a4086a204f079a09a582f4dffec0a
Justin Swanson <geeksunny@gmail.com>
2016-07-03 15:50:58 -0500

    external typeface support wip

---
+++ app/src/main/java/com/radicalninja/transitwear/ui/view/util/FontHelper.java
@@ -0,0 +1,17 @@
+package com.radicalninja.transitwear.ui.view.util;
+
+import android.graphics.Typeface;
+
+import java.util.HashMap;
+
+public class FontHelper {
+
+    private HashMap<String, Typeface> fontMap = new HashMap<>();
+
+//    public static void setBold(final TextView view, final boolean boldEnabled) {
+//        final String fontName = view.getContext().getString(
+//                (boldEnabled) ? R.string.
+//        )
+//    }
+
+}
