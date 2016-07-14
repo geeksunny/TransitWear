b5a9eb788a6816f4add57bc04b522fb75599a427
Justin Swanson <geeksunny@gmail.com>
2016-07-14 00:54:47 -0500

    Initial work on the home fragment.

---
+++ app/src/main/java/com/radicalninja/transitwear/ui/home/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.radicalninja.transitwear.ui.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    private final HomeGridAdapter adapter;
+
+    public HomeFragment() {
+        super();
+        adapter = new HomeGridAdapter(getContext());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final RecyclerView view = new RecyclerView(getContext());
+        final ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        view.setLayoutParams(params);
+
+        view.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        view.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
---
+++ app/src/main/java/com/radicalninja/transitwear/ui/home/HomeGridAdapter.java
@@ -0,0 +1,165 @@
+package com.radicalninja.transitwear.ui.home;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.radicalninja.transitwear.R;
+import com.radicalninja.transitwear.data.model.Route;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeGridAdapter extends RecyclerView.Adapter<HomeGridAdapter.ViewHolder> {
+
+    private static final int HEADER_COUNT = 2;
+
+    private static final int TYPE_UNKNOWN = -1;
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_BUS_ROUTE = 1;
+    private static final int TYPE_TRAIN_ROUTE = 2;
+
+    final Context context;
+    final LayoutInflater inflater;
+    final List<Route> busRoutes = new ArrayList<>();
+    final List<Route> trainRoutes = new ArrayList<>();
+
+    public HomeGridAdapter(Context context) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+    }
+
+    public void addBusRoutes(final List<Route> busRoutes) {
+        this.busRoutes.addAll(busRoutes);
+        notifyDataSetChanged();
+    }
+
+    public void addTrainRoutes(final List<Route> trainRoutes) {
+        this.trainRoutes.addAll(trainRoutes);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return busRoutes.size() + trainRoutes.size() + HEADER_COUNT;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isHeader(position)) {
+            return TYPE_HEADER;
+        } else if (isBusRoute(position)) {
+            return TYPE_BUS_ROUTE;
+        } else if (isTrainRoute(position)) {
+            return TYPE_TRAIN_ROUTE;
+        } else {
+            return TYPE_UNKNOWN;
+        }
+    }
+
+    public Route getItem(int position) {
+        if (isBusRoute(position)) {
+            // tODO: make the -1 offset programmatically calculated
+            return busRoutes.get(position - 1);
+        } else if (isTrainRoute(position)) {
+            return trainRoutes.get(position - busRoutes.size() - 2);
+        } else {
+            return null;
+        }
+    }
+
+    public String getHeader(int position) {
+        if (position == 0) {
+            return context.getString(R.string.header_bus_routes);
+        } else if (position == busRoutes.size() + 1) {
+            return context.getString(R.string.header_train_routes);
+        } else {
+            return context.getString(R.string.header_unknown);
+        }
+    }
+
+    public boolean isHeader(int position) {
+        // Bus header: 1st item
+        // Train header: After bus header + bus routes
+        return position == 0 || position == (busRoutes.size() + 1);
+    }
+
+    public boolean isBusRoute(int position) {
+        return position >= 1 && position < (busRoutes.size() + 1);
+    }
+
+    public boolean isTrainRoute(int position) {
+        return position >= (busRoutes.size() + 2) &&
+                position < (busRoutes.size() + trainRoutes.size() + 2);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final int layoutResId;
+        switch (viewType) {
+            case TYPE_BUS_ROUTE:
+                layoutResId = R.layout.route_list_item;
+                break;
+            case TYPE_TRAIN_ROUTE:
+                layoutResId = R.layout.route_grid_item;
+                break;
+            case TYPE_HEADER:
+            default:
+                layoutResId = R.layout.route_header_item;
+        }
+        final View view = inflater.inflate(layoutResId, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        switch (holder.getItemViewType()) {
+            case TYPE_BUS_ROUTE:
+            case TYPE_TRAIN_ROUTE:
+                holder.setupView(getItem(position));
+                break;
+            case TYPE_HEADER:
+            case TYPE_UNKNOWN:
+            default:
+                holder.setupView(getHeader(position));
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        final TextView title, name;
+        final ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = (ImageView) itemView.findViewById(R.id.route_image);
+            title = (TextView) itemView.findViewById(R.id.route_title);
+            name = (TextView) itemView.findViewById(R.id.route_name);
+        }
+
+        void setupView(final Route route) {
+            // TODO: Set up view data for routes
+            switch (getItemViewType()) {
+                case TYPE_BUS_ROUTE:
+                    title.setText(route.getShortName());
+                case TYPE_TRAIN_ROUTE:
+                    name.setText(route.getLongName());
+                    break;
+                default:
+                    // TODO: handle this.
+                    break;
+            }
+        }
+
+        void setupView(final String title) {
+            // TODO: Set up view data for header.
+            this.title.setText(title);
+        }
+
+    }
+
+}
