1d2a34e81275704c0c396535bada297d6fb3593b
Justin Swanson <geeksunny@gmail.com>
2016-07-18 05:05:15 -0500

    Fixing grid item layouts

--- app/src/main/java/com/radicalninja/transitwear/ui/home/HomeGridAdapter.java
+++ app/src/main/java/com/radicalninja/transitwear/ui/home/HomeGridAdapter.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.radicalninja.transitwear.R;
@@ -200,7 +199,6 @@ public class HomeGridAdapter extends RecyclerView.Adapter<HomeGridAdapter.ViewHo
                     name.setText(route.getLongName());
                     break;
                 case TYPE_TRAIN_ROUTE:
-                    title.setText(route.getShortName());
                     name.setText(route.getLongName());
                     circle.getBackground().setColorFilter(route.getColorHex(), PorterDuff.Mode.MULTIPLY);
                     break;
